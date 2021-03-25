366a396ca2802d66263a9a1089cdb8eb7f0df368
addamsv <swares@mail.ru>
2021-03-25 18:03:01 +0300

    init: state, getLocation and componentDidMount

--- weather/App.js
+++ weather/App.js
@@ -1,13 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 import Loading from './Loading';
 
+class App extends React.Component {
+  state = {
+    isLoading: true,
+  };
+
+  async getLocation() {
+    try {
+      await Location.requestPermissionsAsync();
+      const location = await Location.getCurrentPositionAsync({});
+      return { latitude: location.coords.latitude, longitude: location.coords.longitude };
+    } catch(error) {
+      return new Promise((resolve) => {
+        resolve({latitude: false, longitude: false });
+      });
+    }
+  }
+
+  componentDidMount() {
+    this.getLocation().then((location) => {
+      if (location.latitude && location.longitude) {
+        this.setState({ isLoading: false });
+        console.log(location.latitude, location.longitude);
+      } else {
+        Alert.alert('Cant get Location');
+      }
+    });
+  }
+
+  render() {
+    return (
+      this.state.isLoading ? <Loading /> : null
+    );
+  }
+}
+
+export default App;
+
+/* 
 export default function App() {
   return (
     <Loading />
   );
 }
+*/
+
 /* 
 export default function App() {
   return (
