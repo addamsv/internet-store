663170d7c2368848e4b494e9e7dd782a0265cb6c
addamsv <swares@mail.ru>
2021-03-27 13:39:48 +0300

    feat: linear gradient and second style

--- weather/Weather.js
+++ weather/Weather.js
@@ -1,15 +1,23 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Weather({ temp, condition }) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{ temp }</Text>
-      <Ionicons name="md-checkmark-circle" size={32} color="green" />
-    </View>
+    <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.container}>
+      <StatusBar barStyle='light-content' />
+      <View style={styles.item}>
+        <Ionicons name="rainy" size={96} color='white' />
+        <Text style={styles.text}>{ temp }°C</Text>
+      </View>
+      <View style={{...styles.item, ...styles.downTown}}>
+        <Text style={styles.text}>Rainy</Text>
+        <Text style={styles.subText}>Minsk, Belarus</Text>
+      </View>
+    </LinearGradient>
   );
 }
 
@@ -25,8 +33,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  downTown: {
+    paddingHorizontal: 20,
+  },
   text: {
-    color: '#2c2c2c',
-    fontSize: 30,
+    color: 'white',
+    fontSize: 44,
+    fontWeight: '300',
+    textAlign: 'center',
+  },
+  subText: {
+    color: 'white',
+    fontSize: 24,
+    textAlign: 'center',
   }
 });
