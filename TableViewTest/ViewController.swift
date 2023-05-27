9f54791020e29a00a4eddc36a833c2bbe2ac426a
addamsv <googolcalculator@gmail.com>
2023-05-27 14:58:18 +0300

    Initial Commit

---
+++ TableViewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewTest
+//
+//  Created by Serg on 27.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
