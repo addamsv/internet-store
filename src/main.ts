5308eb017c9f8c5cf998e4d45d8b6740b3c3028e
addamsv <googolcalculator@gmail.com>
2023-11-07 20:12:25 +0300

    init: swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import * as process from "process";
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function nestApp() {
   const PORT = process.env.PORT || 5000;
 
   const app = await NestFactory.create(AppModule);
+
+  const documentation = new DocumentBuilder()
+      .setTitle("The Internet Store")
+      .setDescription("Enhance my \"nest\" tech skills")
+      .setVersion("1.0.0")
+      .addTag("Sergy Addam")
+      .build();
+
+  const doc = SwaggerModule.createDocument(app, documentation);
+
+  SwaggerModule.setup("/api/v1/docs", app, doc);
 
   await app.listen(PORT, () => console.log("ok -> ", PORT));
 }
