1a8189bae6f65ba4fb37e4ef4d525806eb1bd4c7
addamsv <googolcalculator@gmail.com>
2024-01-11 19:24:05 +0300

    refactor: get rid of comments

--- src/brand/brand.model.ts
+++ src/brand/brand.model.ts
@@ -1,15 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  BelongsToMany,
-  Column,
-  DataType,
-  // HasMany,
-  Model,
-  Table,
-} from 'sequelize-typescript';
-// import { Device } from 'src/device/device.model';
-// import { TypeBrand } from 'src/type/type-brand.model';
-// import { Type } from 'src/type/type.model';
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
 @Table({ tableName: 'brand', createdAt: false, updatedAt: false })
 export class Brand extends Model<Brand> {
@@ -24,10 +14,4 @@ export class Brand extends Model<Brand> {
   @ApiProperty({ example: 'Samsung', description: 'Brand of the Device' })
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   name: string;
-
-  // @HasMany(() => Device)
-  // device: Device[];
-
-  // @BelongsToMany(() => Type, () => TypeBrand)
-  // types: Type[];
 }
