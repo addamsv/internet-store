bd89a34b4c2f49c1fd1f53ff139232b80480f190
addamsv <googolcalculator@gmail.com>
2024-03-23 19:23:31 +0300

    feat: entity

--- src/main/java/com/example/demo/endpoints/basket/Basket.java
+++ src/main/java/com/example/demo/endpoints/basket/Basket.java
@@ -1,20 +1,34 @@
 package com.example.demo.endpoints.basket;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
 
+@Entity
 @Table(name = "basket")
 public class Basket {
   @Id
+  @SequenceGenerator(name = "basket_sequence", sequenceName = "basket_sequence", allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "basket_sequence")
+  @Schema(example = "1", description = "Basket ID")
   Long id;
 
+  public Basket() {
+  }
 
+  public Long getId() {
+    return id;
+  }
 
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-
-
-
-
+  @Override
+  public String toString() {
+    return "Basket{" +
+            "id=" + id +
+            '}';
+  }
 
 
 //   @ForeignKey(() => User)
--- src/main/java/com/example/demo/endpoints/order/Order.java
+++ src/main/java/com/example/demo/endpoints/order/Order.java
@@ -1,12 +1,36 @@
 package com.example.demo.endpoints.order;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name = "order")
+@Table(name = "orders")
 public class Order {
     @Id
+    @SequenceGenerator(name = "order_sequence", sequenceName = "order_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_sequence")
+    @Schema(example = "1", description = "Address ID")
     Long id;
+
+    public Order() {
+    }
+
+    public Order(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                '}';
+    }
 }
