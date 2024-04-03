7b85aef7682b6b1bc223ce16e850e8df07477176
addamsv <googolcalculator@gmail.com>
2024-04-03 05:30:12 +0300

    feat: CRUD

--- src/main/java/com/example/demo/endpoints/address/AddressRepository.java
+++ src/main/java/com/example/demo/endpoints/address/AddressRepository.java
@@ -1,8 +1,13 @@
 package com.example.demo.endpoints.address;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    @Query("SELECT a FROM Address a WHERE a.zipCode = ?1 AND a.street = ?2 AND a.houseNumber = ?3")
+    Optional<Address> findByAddress(String zipCode, String street, String houseNumber);
 }
--- src/main/java/com/example/demo/endpoints/address/AddressService.java
+++ src/main/java/com/example/demo/endpoints/address/AddressService.java
@@ -1,26 +1,92 @@
 package com.example.demo.endpoints.address;
 
-import com.example.demo.endpoints.DTO.RespDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class AddressService {
-    public ResponseEntity<RespDTO<List<Address>>> getAll() {
-        return null;
+    private final AddressRepository addressRepository;
+    @Autowired
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
     }
 
-    public ResponseEntity<RespDTO<Address>> create(Address dto) {
-        return null;
+    public ResponseEntity<List<Address>> get() {
+        return new ResponseEntity<>(this.addressRepository.findAll(), HttpStatus.OK);
     }
 
+    public ResponseEntity<Address> create(Address dto) {
+        Optional<Address> candidate = this.addressRepository.findByAddress(dto.getZipCode(), dto.getStreet(), dto.getHouseNumber());
+
+        if (candidate.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(this.addressRepository.save(dto), HttpStatus.CREATED);
+    }
+
+    @Transactional
     public ResponseEntity<String> update(Address dto) {
-        return ResponseEntity.ok("UPDATE Method has not implemented.");
+        Address entity = addressRepository.findById(dto.getId()).orElse(null);
+
+        if (entity == null) {
+            return new ResponseEntity<>("Entity does not exist", HttpStatus.NOT_FOUND);
+        }
+
+        boolean isUpdated = false;
+
+        /* street */
+        if (
+            dto.getStreet() != null && !dto.getStreet().isEmpty()
+            && !Objects.equals(entity.getStreet(), dto.getStreet())
+        ) {
+            entity.setStreet(dto.getStreet());
+        }
+
+        /* houseNumber */
+        if (
+            dto.getHouseNumber() != null && ! dto.getHouseNumber().isEmpty()
+            && ! Objects.equals(entity.getHouseNumber(), dto.getHouseNumber())
+        ) {
+            entity.setHouseNumber(dto.getHouseNumber());
+            isUpdated = true;
+        }
+
+        /* zipCode */
+        if (
+            dto.getZipCode() != null && ! dto.getZipCode().isEmpty()
+            && ! Objects.equals(entity.getZipCode(), dto.getZipCode())
+        ) {
+            System.out.println("========== UPDATED ============");
+            entity.setZipCode(dto.getZipCode());
+            isUpdated = true;
+        }
+
+        if (isUpdated) {
+            /* should be validation same address (address like this is already exist) */
+            return new ResponseEntity<>("Successfully Updated", HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>("There is nothing to update", HttpStatus.BAD_REQUEST);
     }
 
     public ResponseEntity<String> delete(Long id) {
-        return ResponseEntity.ok("DELETE Method has not implemented.");
+        boolean isCandidateExist = addressRepository.existsById(id);
+
+        if (! isCandidateExist) {
+            return new ResponseEntity<>("Entity does not exist", HttpStatus.NOT_FOUND);
+        }
+
+        addressRepository.deleteById(id);
+
+        return new ResponseEntity<>("Removed", HttpStatus.OK);
     }
 }
