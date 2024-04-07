5957712f4718a9700db2fd5eff69097bca7bf34a
addamsv <googolcalculator@gmail.com>
2024-04-07 17:30:16 +0300

    feat: sign-out, activate, refresh token end points

--- src/main/java/com/example/demo/endpoints/auth/AuthController.java
+++ src/main/java/com/example/demo/endpoints/auth/AuthController.java
@@ -3,6 +3,9 @@ package com.example.demo.endpoints.auth;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Auth")
@@ -11,19 +14,110 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin("*")
 public class AuthController {
   private final AuthService authService;
-  public AuthController (AuthService authService) { this.authService = authService; }
 
+  @Autowired
+  public AuthController (AuthService authService) {
+    this.authService = authService;
+  }
+
+  /**
+   * GET activate user by link
+   *
+   * @param link String (PathVariable)
+   * @return AuthResponse
+   */
+  @GetMapping("/activate/{link}")
+  @ApiResponse(responseCode = "200", useReturnTypeSchema = true)
+  @Operation(description = "GET activate user", summary = "GET activate user by LINK")
+  public ResponseEntity<String> activate(@PathVariable("link") String link) {
+    return this.authService.activate(link);
+  }
+
+  /**
+   * GET logout
+   *
+   * @param refreshToken String (from Cookie)
+   * @return AuthResponse
+   */
+  @GetMapping("/sign-out")
+  @ApiResponse(responseCode = "200", useReturnTypeSchema = true)
+  @Operation(description = "GET  Logout", summary = "GET Logout")
+  public ResponseEntity<String> logout(@CookieValue("refresh-token") String refreshToken, HttpServletResponse response) {
+    System.out.println("+=========================================================+");
+    System.out.println("|                refreshToken logout                      |");
+    System.out.println("+=========================================================+");
+    System.out.println(refreshToken);
+
+    this.authService.remRefreshToken(response);
+
+    return this.authService.logout(refreshToken);
+  }
+
+
+
+
+  /**
+   * POST refresh token
+   *
+   * @param refreshToken String (from Cookie)
+   * @return AuthResponse
+   */
+  @PostMapping("/refresh")
+  @ApiResponse(responseCode = "201", useReturnTypeSchema = true)
+  @Operation(description = "REFRESH TOKEN", summary = "REFRESH TOKEN")
+  public AuthRegResponse refresh(@CookieValue("refresh-token") String refreshToken, HttpServletResponse response) {
+    System.out.println("+=========================================================+");
+    System.out.println("|                      old refreshToken                   |");
+    System.out.println("+=========================================================+");
+    System.out.println(refreshToken);
+
+    AuthRegResponse authRegResponse = this.authService.refresh(refreshToken);
+
+    this.authService.setRefreshToken(authRegResponse.getRefreshToken(), response);
+
+    System.out.println("+=========================================================+");
+    System.out.println("|                      new refreshToken                   |");
+    System.out.println("+=========================================================+");
+    System.out.println(authRegResponse.getRefreshToken());
+    return authRegResponse;
+  }
+
+  /**
+   * POST login
+   *
+   * @param request AuthRequest (RequestBody)
+   * @return AuthRegResponse
+   */
   @PostMapping("/login")
   @ApiResponse(responseCode = "201", useReturnTypeSchema = true)
   @Operation(description = "Post", summary = "Login")
-  public AuthResponse login(@RequestBody AuthRequest req) {
-    return this.authService.login(req);
+  public AuthRegResponse login(@RequestBody AuthRequest request, HttpServletResponse response) {
+    AuthRegResponse authRegResponse = this.authService.login(request);
+
+    this.authService.setRefreshToken(authRegResponse.getRefreshToken(), response);
+    System.out.println("+=========================================================+");
+    System.out.println("|                      refreshToken                       |");
+    System.out.println("+=========================================================+");
+    System.out.println(authRegResponse.getRefreshToken());
+
+    return authRegResponse;
   }
 
+
+  /**
+   * POST registration
+   *
+   * @param request AuthRequest (RequestBody)
+   * @return AuthRegResponse
+   */
   @PostMapping("/registration")
   @ApiResponse(responseCode = "201", useReturnTypeSchema = true)
   @Operation(description = "Post", summary = "Registration")
-  public AuthResponse registration(@RequestBody AuthRequest req) {
-    return this.authService.registration(req);
+  public AuthRegResponse registration(@RequestBody AuthRequest request, HttpServletResponse response) {
+    AuthRegResponse authRegResponse = this.authService.registration(request);
+
+    this.authService.setRefreshToken(authRegResponse.getRefreshToken(), response);
+
+    return authRegResponse;
   }
 }
