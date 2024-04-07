3c0ef2965b74e7327a9e78cb5deafad2e7c721df
addamsv <googolcalculator@gmail.com>
2024-04-07 17:46:02 +0300

    feat: assignRole, reAssignRole, ban, check user

--- src/main/java/com/example/demo/endpoints/users/UsersService.java
+++ src/main/java/com/example/demo/endpoints/users/UsersService.java
@@ -1,65 +1,63 @@
 package com.example.demo.endpoints.users;
 
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-
-import com.example.demo.endpoints.DTO.RespDTO;
+import com.example.demo.endpoints.role.Role;
+import com.example.demo.endpoints.role.RoleRepository;
 import com.example.demo.endpoints.users.dto.CreateUserDTO;
 import com.example.demo.endpoints.users.dto.UpdateUserDTO;
+import com.example.demo.guard.jwt.JwtService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
 @Service // the same @Component
 public class UsersService {
 	private final UsersRepository usersRepository;
+	private final RoleRepository roleRepository;
+	private final JwtService jwtService;
 
 	@Autowired
-	public UsersService(UsersRepository usersRepository) {
+	public UsersService(UsersRepository usersRepository, RoleRepository roleRepository, JwtService jwtService) {
 		this.usersRepository = usersRepository;
+		this.roleRepository = roleRepository;
+		this.jwtService = jwtService;
 	}
 
-	public ResponseEntity<RespDTO<Users>> getByLogin(String login) {
+	public ResponseEntity<Users> getByLogin(String login) {
 		Users candidate = usersRepository.findUserByEmail(login).orElse(null);
 
 		if (candidate == null) {
-			return new ResponseEntity<>(
-					new RespDTO<>("Entity does not exist", null),
-					HttpStatusCode.valueOf(409));
-		}
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);}
 
-		return new ResponseEntity<>(
-				new RespDTO<>("SUCCESS", candidate),
-				HttpStatusCode.valueOf(200)
-		);
+		return new ResponseEntity<>(candidate, HttpStatus.OK);
 	}
 
-	public ResponseEntity<RespDTO<List<Users>>> getAll() {
-		return new ResponseEntity<>(
-				new RespDTO<>("SUCCESS", this.usersRepository.findAll()),
-				HttpStatusCode.valueOf(200)
-		);
+	public ResponseEntity<List<Users>> getAll() {
+		return new ResponseEntity<>(this.usersRepository.findAll(), HttpStatus.OK);
 	}
 
-	public ResponseEntity<RespDTO<Users>> create(CreateUserDTO dto) {
+	public ResponseEntity<Users> create(CreateUserDTO dto) {
 		Optional<Users> candidate = this.usersRepository.findUserByEmail(dto.getEmail());
 
 		if (candidate.isPresent()) {
-			return new ResponseEntity<>(
-					new RespDTO<>("Entity is already exist", null),
-					HttpStatusCode.valueOf(409));
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
 		}
 
-		Users user = new Users(dto.getPassword(), dto.getEmail(), dto.getRole());
-		return new ResponseEntity<>(
-				new RespDTO<>("Created", this.usersRepository.save(user)),
-				HttpStatusCode.valueOf(201)//HttpStatus.CREATED
-		);
+		Role role = roleRepository.findRoleByValue("USER").orElse(new Role(7777777777L,"USER-COMMON", "AVERAGE"));
+
+		Users user = new Users(dto.getPassword(), dto.getEmail());
+
+		user.setRoles(List.of(role));
+
+		return new ResponseEntity<>(this.usersRepository.save(user), HttpStatus.CREATED);
 	}
 
 	@Transactional
@@ -77,14 +75,6 @@ public class UsersService {
 		) {
 			/* Hash */
 			entity.setPassword(dto.getPassword());
-			isUpdated = true;
-		}
-
-		if (
-			dto.getRole() != null && ! dto.getRole().isEmpty()
-			&& ! Objects.equals(entity.getRole(), dto.getRole())
-		) {
-			entity.setRole(dto.getRole());
 			isUpdated = true;
 		}
 
@@ -121,9 +111,114 @@ public class UsersService {
 		return new ResponseEntity<>("Removed", HttpStatusCode.valueOf(200));
 	}
 
-	public boolean check(HttpServletRequest req) {//ResponseEntity<RespDTO<Users>>
-		System.out.println(req.getHeader("Authorization"));
-		return false;
-//		return new ResponseEntity<>(new RespDTO<>("Success", null), HttpStatus.OK);
+	public boolean check(HttpServletRequest req) {
+		final String authHeader = req.getHeader("Authorization");
+
+		if (authHeader == null || ! authHeader.startsWith("Bearer ")) {
+			return false;
+		}
+
+		final String token = authHeader.substring(7);
+
+		final String email = jwtService.extractUsername(token);
+
+		final Users user = usersRepository.findUserByEmail(email).orElse(null);
+
+		if (user == null) {
+			return  false;
+		}
+
+		return jwtService.isTokenValid(token, user);
+	}
+
+	public ResponseEntity<Users> assignRole(Long id, String roleName) {
+		Users users = this.usersRepository.findById(id).orElse(null);
+
+		if (users == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+		Role role = roleRepository.findRoleByValue(roleName).orElse(null);
+
+		if (role == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+
+		/* validation IS_ROLE_ALREADY_EXIST */
+		List<Role> roles = users.getRoles();
+
+		boolean isRoleAlreadyExist = false;
+
+		for (Role r : roles) {
+			if (Objects.equals(r.getValue(), role.getValue())) {
+				isRoleAlreadyExist = true;
+			}
+		}
+
+		if (isRoleAlreadyExist) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+
+		roles.add(role);
+		users.setRoles(roles);
+
+		// Next line will insert on USERS and ROLE_USERS tables
+		users = usersRepository.saveAndFlush(users);
+
+		List<Users> usersList = role.getUsers();
+		usersList.add(users);
+
+		// We add the Users to the Role object to keep both sides in sync
+		role.setUsers(usersList); // should add the user (not override)
+
+		return new ResponseEntity<>(users, HttpStatus.OK);
+	}
+
+	public ResponseEntity<Users> reAssignRole(Long id, String roleName) {
+		Users users = this.usersRepository.findById(id).orElse(null);
+
+		if (users == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+		Role role = roleRepository.findRoleByValue(roleName).orElse(null);
+
+		if (role == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+
+		/* validation IS_ROLE_ALREADY_EXIST */
+		List<Role> roles = users.getRoles();
+
+		boolean isRoleAlreadyExist = false;
+
+		for (Role r : roles) {
+			if (Objects.equals(r.getValue(), role.getValue())) {
+				isRoleAlreadyExist = true;
+			}
+		}
+
+		if (isRoleAlreadyExist) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+		return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+	}
+
+//	@Transactional
+	public ResponseEntity<Users> ban(Long id, String reason) {
+		Users users = this.usersRepository.findById(id).orElse(null);
+
+		if (users == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+		users.setBanned(true);
+		users.setBanReason(reason);
+
+		return new ResponseEntity<>(users, HttpStatus.OK);
 	}
 }
