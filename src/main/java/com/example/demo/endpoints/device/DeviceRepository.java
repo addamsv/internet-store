11182a7dde0ffa9a84e175d3af1405897362dd99
addamsv <googolcalculator@gmail.com>
2024-04-07 17:32:36 +0300

    feat: findAndCountByBrand, findAndCountByType, findAndCountByBrandAndType

--- src/main/java/com/example/demo/endpoints/device/DeviceRepository.java
+++ src/main/java/com/example/demo/endpoints/device/DeviceRepository.java
@@ -14,9 +14,18 @@ public interface DeviceRepository extends JpaRepository<Device, Long> {
     Optional<Device> findDeviceByName(String name);
 
     // SELECT * FROM "device" WHERE "id" = ?; //INCLUDE [{ model: DeviceInfo, as: 'deviceInfo' }]
-    @Query("SELECT d FROM Device d WHERE d.id = ?1")
+    @Query("SELECT d FROM Device d WHERE d.id = ?1") // AND d.deviceInfo.id
     Optional<Device> findByIdPlusDeviceInfo(Long id);
 
     @Query("SELECT d FROM Device d ORDER BY d.id ASC LIMIT ?1 OFFSET ?2")
-    List<Device> findAndCountAll(Integer limit, Integer offset);
+    List<Device> findAndCountAll(int limit, int offset);
+
+    @Query("SELECT d FROM Device d WHERE d.brand.id = ?1 ORDER BY d.id ASC LIMIT ?2 OFFSET ?3")
+    List<Device> findAndCountByBrand(Long brandId, int limit, int offset);
+
+    @Query("SELECT d FROM Device d WHERE d.type.id = ?1 ORDER BY d.id ASC LIMIT ?2 OFFSET ?3")
+    List<Device> findAndCountByType(Long typeId, int limit, int offset);
+
+    @Query("SELECT d FROM Device d WHERE d.brand.id = ?1 AND d.type.id = ?2 ORDER BY d.id ASC LIMIT ?3 OFFSET ?4")
+    List<Device> findAndCountByBrandAndType(Long brandId, Long typeId, int limit, int offset);
 }
