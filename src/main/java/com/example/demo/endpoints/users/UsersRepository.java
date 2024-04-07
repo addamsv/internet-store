e82e1fb63d0993078db8622e2d8c876227808836
addamsv <googolcalculator@gmail.com>
2024-04-07 17:44:39 +0300

    feat: findByActivationLink, findByRefreshToken

--- src/main/java/com/example/demo/endpoints/users/UsersRepository.java
+++ src/main/java/com/example/demo/endpoints/users/UsersRepository.java
@@ -9,10 +9,13 @@ import java.util.Optional;
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long>{
 
-//    @Query("Select * from table_name where column_name  like %:keyword% ALLOW FILTERING ")
     // SELECT * FROM "users" WHERE "email" = ?;
     @Query("SELECT u FROM Users u WHERE u.email = ?1")
     Optional<Users> findUserByEmail(String email);
-//    Users findOneByEmail(String email);
 
+    @Query("SELECT u FROM Users u WHERE u.activationLink = ?1")
+    Optional<Users> findByActivationLink(String link);
+
+    @Query("SELECT u FROM Users u WHERE u.refreshToken = ?1")
+    Optional<Users> findByRefreshToken(String refreshToken);
 }
