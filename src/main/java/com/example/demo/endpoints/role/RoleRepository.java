a71b193a5e652271182a86e1987b41a39f8bbd61
addamsv <googolcalculator@gmail.com>
2024-04-07 17:39:53 +0300

    refactor: minor update

--- src/main/java/com/example/demo/endpoints/role/RoleRepository.java
+++ src/main/java/com/example/demo/endpoints/role/RoleRepository.java
@@ -1,4 +1,4 @@
-package com.example.demo.endpoints.roles;
+package com.example.demo.endpoints.role;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +11,8 @@ public interface RoleRepository extends JpaRepository<Role, Long> {
     // SELECT * FROM "brand" WHERE "name" = ?;
     @Query("SELECT r FROM Role r WHERE r.value = ?1")
     Optional<Role> findRoleByValue(String value);
+
+//    @Query(value = "SELECT r FROM Roles r INNER JOIN UsersRoles ur ON r.id = ur.roleId WHERE ur.users_id = ?1 ")
+//    @Query(value = "select r.* from roles r inner join users_roles ur on r.id = ur.users_id", nativeQuery = true)
+//    List<Role> findAllByUserId(long userId);
 }
