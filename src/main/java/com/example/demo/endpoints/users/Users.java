28e7e89ec33a7585cdc0b2dfe3a9398ce6e07ca8
addamsv <googolcalculator@gmail.com>
2024-04-07 17:42:43 +0300

    feat: isActivated, activationLink, refreshToken, isBanned, banReason & many-to-many rel

--- src/main/java/com/example/demo/endpoints/users/Users.java
+++ src/main/java/com/example/demo/endpoints/users/Users.java
@@ -1,12 +1,17 @@
 package com.example.demo.endpoints.users;
 
 import com.example.demo.endpoints.address.Address;
+import com.example.demo.endpoints.role.Role;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -25,10 +30,37 @@ public class Users implements UserDetails {
 	@Column(name = "email", nullable = false, unique = true)
 	private String email;
 
-	@Schema(example = "ADMIN", description = "Users role")
-	@Column(name = "role", nullable = false)
-	private String role = "ADMIN";
+	@Schema(example = "false", description = "Is User Activated")
+	@Column(name = "is_activated", nullable = false)
+	private Boolean isActivated = false;
 
+	@Schema(example = "xcvcbv-asdb-nmhj-kasd-fgdfg-yuty", description = "Users Activation Link")
+	@Column(name = "activation_link", nullable = false)
+	private String activationLink = "";
+
+	@Schema(example = "xcvcbvasdb.nmhjkasd.fgdfgyuty", description = "Users Refresh Token")
+	@Column(name = "refresh_token", nullable = false, columnDefinition="text")
+	private String refreshToken = "";
+
+	@Schema(example = "false", description = "Is User Banned")
+	@Column(name = "is_banned", nullable = false)
+	private Boolean isBanned = false;
+
+	@Schema(example = "For Cheating", description = "Reason of Ban of the User")
+	@Column(name = "ban_reason", nullable = true)
+	private String banReason;
+
+
+	/* Owner */
+	@ManyToMany(fetch = FetchType.EAGER)
+	/* in order to avoid a circular calls */
+	@JsonBackReference
+	@JoinTable(
+			name = "users_roles",
+			joinColumns = @JoinColumn(name = "users_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id")
+	)
+	private List<Role> roles;
 
 
 
@@ -38,33 +70,21 @@ public class Users implements UserDetails {
 	private Address address;
 
 
-
-
-
-	@Override
-	public String toString() {
-		return "Users{" +
-				"id=" + id +
-				", password='" + password + '\'' +
-				", email='" + email + '\'' +
-				", role='" + role + '\'' +
-				", address=" + address +
-				'}';
-	}
-
 	public Users() {}
 
-	public Users(String email, String password, String role) {
-		this.password = password;
-		this.email = email;
-		this.role = role;
+	public Users(Long id) {
+		this.id = id;
 	}
 
-	public Users(Long id, String email, String password, String role) {
-		this.id = id;
-		this.password = password;
+	public Users(String email, String password) {
 		this.email = email;
-		this.role = role;
+		this.password = password;
+	}
+
+	public Users(Long id, String email, String password, String activationLink) {
+		this.id = id;
+		this.email = email;
+		this.password = password;
 	}
 
 	public Long getId() {
@@ -72,6 +92,47 @@ public class Users implements UserDetails {
 	}
 
 	public void setId(Long id) { this.id = id; }
+
+
+	public Boolean getActivated() {
+		return isActivated;
+	}
+
+	public void setActivated(Boolean activated) {
+		isActivated = activated;
+	}
+
+	public String getActivationLink() {
+		return activationLink;
+	}
+
+	public void setActivationLink(String activationLink) {
+		this.activationLink = activationLink;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+	public Boolean getBanned() {
+		return isBanned;
+	}
+
+	public void setBanned(Boolean banned) {
+		isBanned = banned;
+	}
+
+	public String getBanReason() {
+		return banReason;
+	}
+
+	public void setBanReason(String banReason) {
+		this.banReason = banReason;
+	}
 
 	public Address getAddress() {
 		return address;
@@ -81,10 +142,21 @@ public class Users implements UserDetails {
 		this.address = address;
 	}
 
+
+
+
+
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return null;
+		List<GrantedAuthority> list = new ArrayList<>();
+
+		for (Role role : roles) {
+			list.add(new SimpleGrantedAuthority("ROLE_" + role.getValue()));
+		}
+
+		return list;
 	}
+
 
 	public String getPassword() {
 		return password;
@@ -127,12 +199,27 @@ public class Users implements UserDetails {
 		this.email = email;
 	}
 
-	public String getRole() {
-		return role;
+	public String getRefreshToken() {
+		return refreshToken;
 	}
 
-	public void setRole(String role) {
-		this.role = role;
+	public void setRefreshToken(String refreshToken) {
+		this.refreshToken = refreshToken;
 	}
 
+	@Override
+	public String toString() {
+		return "Users{" +
+				"id=" + id +
+				", password='" + password + '\'' +
+				", email='" + email + '\'' +
+				", isActivated=" + isActivated +
+				", activationLink='" + activationLink + '\'' +
+				", refreshToken='" + refreshToken + '\'' +
+				", isBanned=" + isBanned +
+				", banReason='" + banReason + '\'' +
+				", roles=" + roles +
+				", address=" + address +
+				'}';
+	}
 }
