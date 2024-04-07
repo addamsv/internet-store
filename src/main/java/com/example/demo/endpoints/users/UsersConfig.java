eddc1617e56ddf26116a67e55e8eece03b0a83c6
addamsv <googolcalculator@gmail.com>
2024-04-07 17:43:44 +0300

    feat: many-to-many rel

--- src/main/java/com/example/demo/endpoints/users/UsersConfig.java
+++ src/main/java/com/example/demo/endpoints/users/UsersConfig.java
@@ -1,12 +1,16 @@
 package com.example.demo.endpoints.users;
 
+import com.example.demo.endpoints.role.Role;
+import com.example.demo.endpoints.role.RoleRepository;
 import com.example.demo.guard.jwt.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Configuration
 public class UsersConfig {
@@ -17,21 +21,49 @@ public class UsersConfig {
     }
 
     @Bean
-    CommandLineRunner commandLineRunner(UsersRepository usersRepository) {
+    CommandLineRunner commandLineRunner(UsersRepository usersRepository, RoleRepository roleRepository) {
         return args -> {
-            Users sergey = new Users(
-                    "sergy@a.a",
-                    jwtService.getPassHash("Sergey-aaa"),
-                    "ADMIN"
+            Role role = roleRepository.save(
+                new Role("ADMIN", "Administrator")
             );
 
-            Users alexandr = new Users(
+            Users users = new Users(
+                "sergy@a.a",
+                jwtService.getPassHash("Sergey-aaa")
+            );
+
+            users.setRoles(List.of(role));
+            users.setActivationLink(UUID.randomUUID().toString());
+
+            // Next line will insert on USERS and ROLE_USERS tables
+            users = usersRepository.saveAndFlush(users);
+
+            // We add the category to the ad object to keep both sides in sync
+            role.setUsers(List.of(users));
+
+
+
+
+
+
+            role = roleRepository.save(
+                    new Role("USER", "Average User")
+            );
+
+            users = new Users(
                     "alex@a.a",
-                    jwtService.getPassHash("Alex-bbb"),
-                    "USER"
+                    jwtService.getPassHash("aaa")
             );
 
-            usersRepository.saveAll(List.of(sergey, alexandr));
+            users.setRoles(List.of(role));
+            users.setActivationLink(UUID.randomUUID().toString());
+
+            // Next line will insert on USERS and ROLE_USERS tables
+            users = usersRepository.saveAndFlush(users);
+
+            // We add the category to the ad object to keep both sides in sync
+            role.setUsers(List.of(users));
+
         };
     }
 }
